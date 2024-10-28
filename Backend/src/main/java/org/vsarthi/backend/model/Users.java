2bc0140e112986ddd609e7e392bdc048a0ec81b6
Aditya-2407-G <adi.gupta2407@gmail.com>
2024-10-29 00:03:51 +0530

    .

--- Backend/src/main/java/org/vsarthi/backend/model/Users.java
+++ Backend/src/main/java/org/vsarthi/backend/model/Users.java
@@ -2,7 +2,6 @@ package org.vsarthi.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.util.List;
